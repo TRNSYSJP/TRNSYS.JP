0d4faa795336e9e145c35bc9d9bd79840e909ef4
yuizi <yuichi.yasuda@gmail.com>
2021-11-15 12:52:24 +0900

    Type169のサンプルを追加

---
+++ TRNSYS18/MyProjects/Type169_sine_curve/sin_curve.py
@@ -0,0 +1,14 @@
+"""type169(Python) sample"""
+import TRNSYSpy as trn
+import math
+
+def PythonFunction():
+	# retrieve values from the Inputs
+    inp1 = trn.getInputValue(1)
+	# processing
+    degree = inp1%360
+    out1 = math.sin(math.radians(degree))
+    #out1=inp1*2
+	# return the new values to the Outputs
+    trn.setOutputValue(1, out1)    
+    return
