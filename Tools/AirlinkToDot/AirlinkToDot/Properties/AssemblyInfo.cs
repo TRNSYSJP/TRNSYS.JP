d66fc1534a58752b46a97bc31ae074358728231e
yuizi <yuichi.yasuda@gmail.com>
2022-01-12 21:18:33 +0900

    GraphViz2.50対応

--- Tools/AirlinkToDot/AirlinkToDot/Properties/AssemblyInfo.cs
+++ Tools/AirlinkToDot/AirlinkToDot/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("quattro corporate design Co., Ltd.")]
 [assembly: AssemblyProduct("AirlinkToDot")]
-[assembly: AssemblyCopyright("Copyright © quattro corporate design Co., Ltd. 2018")]
+[assembly: AssemblyCopyright("Copyright © quattro corporate design Co., Ltd. 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.7.2")]
-[assembly: AssemblyFileVersion("1.0.7.2")]
+[assembly: AssemblyVersion("1.0.8.0")]
+[assembly: AssemblyFileVersion("1.0.8.0")]
